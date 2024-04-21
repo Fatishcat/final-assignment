7c4cfb413c1de6323315bedd4261d8f4fc199e08
Fatishcat <157142616+Fatishcat@users.noreply.github.com>
2024-04-21 14:34:58 -0400

    started have no idea what i'm doig

---
+++ MorseTester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class MorseTester {
+
+	public static void main(String[] args) {
+		try {
+			File dataFile = new File("morsecode.txt");
+			Scanner scnr = new Scanner(dataFile);
+
+		} catch (FileNotFoundException e) {
+		    System.out.println("An error occurred.");
+		    e.printStackTrace();
+		  }
+		
+		M
+	}
+
+}
